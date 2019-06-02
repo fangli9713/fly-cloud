89446d5a1fec2044b693e15da19a59a531763001
fangli9713 <971399161@qq.com>
2019-06-02 22:46:47 +0800

    优化

--- robot/src/main/java/com/fly/robot/service/TuRingRobotService.java
+++ robot/src/main/java/com/fly/robot/service/TuRingRobotService.java
@@ -1,5 +1,9 @@
 package com.fly.robot.service;
 
+import com.fly.common.dto.BaseResult;
+
 public interface TuRingRobotService {
+
+    BaseResult talk(String word);
 
 }
--- robot/src/main/java/com/fly/robot/service/impl/TuRingRobotServiceImpl.java
+++ robot/src/main/java/com/fly/robot/service/impl/TuRingRobotServiceImpl.java
@@ -1,7 +1,16 @@
 package com.fly.robot.service.impl;
 
 
+import com.fly.common.dto.BaseResult;
 import com.fly.robot.service.TuRingRobotService;
 
 public class TuRingRobotServiceImpl implements TuRingRobotService {
+
+    public static final String apiUrl = "http://openapi.tuling123.com/openapi/api/v2";
+
+    @Override
+    public BaseResult talk(String word) {
+
+        return null;
+    }
 }
