b95cd45b79ea700512c250ea1da04941b9c2a8ad
fanglinan <fanglinan@shenzhenpoly.com>
2019-06-05 18:12:01 +0800

    优化

--- robot/src/main/java/com/fly/robot/RobotApplication.java
+++ robot/src/main/java/com/fly/robot/RobotApplication.java
@@ -13,8 +13,8 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
   * @description: robot工程的启动入口
@@ -23,16 +23,17 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
   */
 
 @SpringBootApplication
+@EnableTransactionManagement
+@EnableDruidSourceConfiguration
 @ServletComponentScan
 @EnableCaching
-@EnableZuulProxy
 @EnableDiscoveryClient
 @EnableFeignClients
 //@EnableOAuth2Sso
 @RefreshScope
 @Slf4j
 @EnableCorsConfiguration
-@EnableDruidSourceConfiguration
+
 public class RobotApplication implements CommandLineRunner {
 
     @Value("${spring.profiles.active}")
