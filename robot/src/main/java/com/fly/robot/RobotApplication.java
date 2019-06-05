07320d12533c52cb99f6456e696802d9067ac8dc
fanglinan <fanglinan@shenzhenpoly.com>
2019-06-05 18:40:25 +0800

    优化

--- robot/src/main/java/com/fly/robot/RobotApplication.java
+++ robot/src/main/java/com/fly/robot/RobotApplication.java
@@ -9,11 +9,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
@@ -23,17 +22,14 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
   */
 
 @SpringBootApplication
-@EnableTransactionManagement
-@EnableDruidSourceConfiguration
-@ServletComponentScan
-@EnableCaching
 @EnableDiscoveryClient
 @EnableFeignClients
-//@EnableOAuth2Sso
+@EnableTransactionManagement
 @RefreshScope
-@Slf4j
+@EnableAsync
 @EnableCorsConfiguration
-
+@EnableDruidSourceConfiguration
+@Slf4j
 public class RobotApplication implements CommandLineRunner {
 
     @Value("${spring.profiles.active}")
