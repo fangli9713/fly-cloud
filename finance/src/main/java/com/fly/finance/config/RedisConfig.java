646aa37ce27cc4dd1658349f320a0d9f12256e26
fanglinan <fanglinan@shenzhenpoly.com>
2019-08-22 14:01:19 +0800

    增加ui

---
+++ finance/src/main/java/com/fly/finance/config/RedisConfig.java
@@ -0,0 +1,24 @@
+package com.fly.finance.config;
+
+import com.fly.common.config.redis.DefaultAutoRedisConfig;
+import com.fly.common.util.redis.RedisUtil;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class RedisConfig extends DefaultAutoRedisConfig {
+
+    @Bean
+    @Override
+    public RedisTemplate defaultRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        return super.defaultRedisTemplate(redisConnectionFactory);
+    }
+
+    @Bean
+    public RedisUtil redisUtil(RedisTemplate defaultRedisTemplate) {
+        return new RedisUtil(defaultRedisTemplate);
+    }
+
+}
